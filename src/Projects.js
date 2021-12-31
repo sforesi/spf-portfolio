1728b15ab476ce8465022b96dfa7e96e43bbde01
sforesi <sebastiano.foresi@gmail.com>
2021-12-31 11:34:55 -0500

    stub up thumbnails

--- src/Projects.js
+++ src/Projects.js
@@ -1,9 +1,18 @@
 import React from "react"
+import Thumbnail from './Thumbnail.js';
+import './App.css';
+
 
 function Projects(props) {
   return (
     <div>
       <h1>Projects</h1>
+      <Thumbnail
+        link="/heroku"
+        image="https://tilt-games.herokuapp.com/tilt.png"
+        title="tilt"
+        category="Desktop App"
+      />
     </div>
   )
 }

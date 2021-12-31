9ec556b1c614d2dfbec8d4dd372d6f530f1dadd2
sforesi <sebastiano.foresi@gmail.com>
2021-12-31 10:01:28 -0500

    stub up components

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="navigation">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a 
+        href='' 
+        className='item'>
+          Projects
         </a>
-      </header>
+        <a 
+        href='' 
+        className='item'>
+          Resume
+        </a>
+        <a 
+        href='' 
+        className='item'>
+          About
+        </a>
+      </div>
     </div>
   );
 }

e0ca94110421d0804b1b76439f037014c1cdf6ee
sforesi <sebastiano.foresi@gmail.com>
2021-12-31 10:33:22 -0500

    localhost and routes working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Link } from 'react-router-dom'; 
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom'; 
 import Projects from './Projects.js'; 
 import Resume from './Resume.js'; 
 import About from './About.js'; 
@@ -10,23 +10,22 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
+
+      <Routes>
+        <Route exact path="/" component={Projects} />
+          <Route path="/resume" component={Resume} />
+          <Route path="/about" component={About} />
+      </Routes>
+      
         <div className="navigation">
           <img src={logo} className="App-logo" alt="logo" />
-          <a 
-          href='' 
-          className='item'>
-            Projects
-          </a>
-          <a 
-          href='' 
-          className='item'>
-            Resume
-          </a>
-          <a 
-          href='' 
-          className='item'>
-            About
-          </a>
+          <div className='navigation-sub'>
+
+          <Link to="/" className="item">Projects</Link>
+            <Link to="/resume" className="item">Resume</Link>
+            <Link to="/about" className="item">About</Link>
+
+          </div>
         </div>
       </div>
     </BrowserRouter>

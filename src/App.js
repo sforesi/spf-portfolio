742676c0bedd556346b4520b7f037737dc7ab94c
sforesi <sebastiano.foresi@gmail.com>
2021-12-31 10:08:03 -0500

    import route, link and browser router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom'; 
+import Projects from './Projects.js'; 
+import Resume from './Resume.js'; 
+import About from './About.js'; 
+
 
 function App() {
   return (
-    <div className="App">
-      <div className="navigation">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a 
-        href='' 
-        className='item'>
-          Projects
-        </a>
-        <a 
-        href='' 
-        className='item'>
-          Resume
-        </a>
-        <a 
-        href='' 
-        className='item'>
-          About
-        </a>
+    <BrowserRouter>
+      <div className="App">
+        <div className="navigation">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a 
+          href='' 
+          className='item'>
+            Projects
+          </a>
+          <a 
+          href='' 
+          className='item'>
+            Resume
+          </a>
+          <a 
+          href='' 
+          className='item'>
+            About
+          </a>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
